920946e9b3853dfeb732c5d53e365271cef942b0
Priyanshu-Singh2002 <142142234+Priyanshu-Singh2002@users.noreply.github.com>
2025-06-11 00:01:42 +0530

    config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 #configure the mysql db connection
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Password@localhost/priyanshu_career'
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SESSION_COOKIE_SECURE = True  
